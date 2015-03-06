865e6fbf471857497f175bb54028be05382cf1d3
irisli <_l@berkeley.edu>
2015-03-05 19:15:38 -0800

    switch over to AMD.js style define

--- modules/core.js
+++ modules/core.js
@@ -1,10 +1,7 @@
-$.fn.stories.modules['navButtons'] = function() {
+stories.define('navButtons', ['background--slide'], function() {
   var module = this;
 
   return {
-    deps: [
-      "background--slide",
-    ],
     entry: function() {
       this.$ui.prepend('<div class="navigation-buttons"><a class="nav-prev"></a><a class="nav-next"></a></div>');
       this.$navButtons = this.$ui.find('> .navigation-buttons');
@@ -13,23 +10,20 @@ $.fn.stories.modules['navButtons'] = function() {
       this.$navButtons.find('.nav-next').click(this.nextSlide.bind(this));
     },
   };
-}
+});
 
 // Background based on the current slide
-$.fn.stories.modules['background--slide'] = function() {
+stories.define('background--slide', [], function() {
   var module = this;
   module.newSlide = function(e, targetSlide) {
     this.$background.attr({'template': $(targetSlide).attr('template')});
   };
 
   return {
-    deps: [
-      "",
-    ],
     entry: function() {
       this.$ui.prepend('<div class="background"></div>');
       this.$background = this.$ui.find('> .background');
       this.on("slide.change", module.newSlide.bind(this));
     },
   };
-}
+});
