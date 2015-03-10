f3c402c2986db412a0fc02b5b85150cd24015618
irisli <_l@berkeley.edu>
2015-03-10 15:51:18 -0700

    gulp task for compiling stories

--- README.md
+++ README.md
@@ -3,6 +3,13 @@ Stellar Stories
 Stellar Stories is a collection of graphic explainers used to help people learn
 more about Stellar and what it can do.
 
+## Getting started
+0. Assuming that you have already installed node and npm
+1. Install gulp globally
+```
+npm install --global gulp
+```
+
 ## Content
 The `content` folder contains stories and the files necessary to view them.
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,53 @@
+var fs = require('fs');
+var path = require('path');
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var map = require('vinyl-map');
+
 
 gulp.task('default', function() {
-  console.log('default')
+  console.log('Use gulp build');
 });
 
 gulp.task('sass', function() {
   gulp.src('./styles/scss/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./styles/css'))
-    .pipe(gulp.dest('./dist/assets/css'))
+    .pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('compile-stories', function() {
+  var template = fs.readFileSync('./story-app.html').toString(); // TODO: make this async
+  var content = gulp.src('./content/**/config.json');
+
+  var inlineConfig = map(function(content) {
+    content = content.toString();
+    content = "<script>window.storyConfig = " + content + ";</script>"
+    return template.replace('<!-- config inserted here -->', content);
+  });
+
+  var inlineSlide = map(function(content, filename) {
+    var slidesPath = path.dirname(filename) + "/slides.html"
+    var slidesContent = fs.readFileSync(slidesPath, 'utf-8'); // TODO: make this async
+    content = content.toString();
+    return content.replace('<!-- slides inserted here -->', slidesContent);
+  });
+
+  content
+    .pipe(inlineConfig)
+    .pipe(inlineSlide)
+    .pipe(rename(function(path) {
+      path.basename = 'index';
+      path.extname = '.html';
+    }))
+    .pipe(gulp.dest('./dist'));
+
+  gulp.src('./content/**/assets/*')
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('build', ['compile-stories', 'sass'], function() {
+
 });
