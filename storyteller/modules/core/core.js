d915bc043abadf9094fbdbb778c5beee0fa6cdd6
irisli <_l@berkeley.edu>
2015-04-07 22:19:56 -0700

    add queue requirements for segment

--- storyteller/modules/core/core.js
+++ storyteller/modules/core/core.js
@@ -1326,27 +1326,66 @@ storyteller.define('analytics-segment', function() {
   self.options = {
   };
 
+  self.eventQueue = [];
+  self.eventQueueFulfilled = false;
+  self.windowFocused = false;
+  self.initHappened = false;
+  self.analyticsEnabled = false;
+
+  self.updateEventQueue = function() {
+    if (self.windowFocused && self.initHappened && self.analyticsEnabled && !self.eventQueueFulfilled) {
+      self.eventQueueFulfilled = true;
+      self.initSegment();
+      for (var i = 0; i < self.eventQueue.length; i++) {
+        analytics.track(self.eventQueue[i], self.properties);
+      }
+    }
+  };
+
+  self.initSegment = function() {
+    // taken from segment
+    !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.0.1";
+    analytics.load(self.options.writeKey);
+    analytics.page()
+    }}();
+  };
+
   return {
     tools: ['events', 'options'],
     entry: function(tools) {
       t = tools;
-      t.events.on('analytics:enabled', function() {
-        $.extend(self.options, t.options.analyticsSegment);
-        var properties = {};
-        // TODO: more configurable properties
-        if (self.options.hasOwnProperty('category')) {
-          properties.category = self.options.category;
+
+      $.extend(self.options, t.options.analyticsSegment);
+      self.properties = {};
+      // TODO: more configurable properties
+      if (self.options.hasOwnProperty('category')) {
+        self.properties.category = self.options.category;
+      }
+
+      t.events.on('analytics:event', function(e, event) {
+        // TODO: Make this nicer
+        if (!self.eventQueueFulfilled) {
+          self.eventQueue.push(event.name);
+          self.updateEventQueue();
+        } else {
+          analytics.track(event.name, self.properties);
         }
+      });
 
-        // taken from segment
-        !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.0.1";
-        analytics.load(self.options.writeKey);
-        analytics.page()
-        }}();
-
-        t.events.on('analytics:event', function(e, event) {
-          analytics.track(event.name, properties);
-        });
+      // TODO: remove when done
+      setInterval(function() {
+        if (document.hasFocus()) {
+          self.windowFocused = true;
+          self.updateEventQueue();
+        }
+      }, 200);
+      t.events.on('init', function() {
+        self.initHappened = true;
+        self.updateEventQueue();
+      });
+      t.events.on('analytics:enabled', function() {
+        self.analyticsEnabled = true;
+        self.updateEventQueue();
       });
     },
   };
